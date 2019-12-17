4bcca9c9404d80b190ea15d9a06ffce9825da90c
Adewale Azeez <azeezadewale98@gmail.com>
2019-12-17 19:21:25 +0100

    make color class have color attr and bg-color have background color

--- language-color.ps1
+++ language-color.ps1
@@ -1,10 +1,14 @@
 
+if (-not (Test-Path "test/")){
+    mkdir  test/
+}
 $LinguistGrammerUrl = "https://rawgit.com/github/linguist/master/lib/linguist/languages.yml"
-$Content = Invoke-WebRequest $LinguistGrammerUrl
+$Content = Invoke-WebRequest -Uri $LinguistGrammerUrl -UseBasicParsing
 $Content = $Content.ToString().Replace('`r', '') -split "`n"
-#"$Content" | out-file -filepath test/languages.yml
+"$Content" | out-file -filepath test/languages.yml
 $CurrentColorClass = "/** `n`tAdewale Azeez <azeezadewale98@gmail.com>`n`tMIT License`n`tCopyright (c) 2019 Quick Utils`n`t$((Get-Date).ToString())`n**/"
 $CurrentColorClass | out-file -filepath language-color.css
+$CurrentBGColorClass = ""
 ForEach ($Line in $Content)
 {
 	if ($Line.StartsWith("#") -or $Line.StartsWith("-") -or $Line.Trim() -eq "") 
@@ -17,18 +21,22 @@ ForEach ($Line in $Content)
 		{
 			$FirstIndex = $Line.IndexOf('"')
 			$ColorValue = $Line.SubString($FirstIndex + 1, $Line.LastIndexOf('"') - $FirstIndex - 1)
-			$CurrentColorClass = "$CurrentColorClass background-color: $ColorValue; }"
+			$CurrentColorClass = "$CurrentColorClass color: $ColorValue; }"
+			$CurrentBGColorClass = "$CurrentBGColorClass background-color: $ColorValue; }"
 		}
 	} 
 	else 
 	{
-		if (-not $CurrentColorClass -eq "" -and $CurrentColorClass.Contains("background-color")) 
+		if (-not $CurrentBGColorClass -eq "" -and $CurrentBGColorClass.Contains("background-color")) 
 		{
 			$CurrentColorClass | out-file -append -filepath language-color.css
+			$CurrentBGColorClass | out-file -append -filepath language-color.css
 		}
 		$CurrentColorClass = ""
+		$CurrentBGColorClass = ""
 		$Line = $Line.Replace('+', 'p').Replace("'", '').Replace("#", 'sharp').Replace("*", 'star').Replace("!", 'not')
 		$LanguageClassName = $Line.Replace(':', '').Replace(' ', '-').ToLower().Trim()
 		$CurrentColorClass = ".color-$LanguageClassName {"
+		$CurrentBGColorClass = ".bg-color-$LanguageClassName {"
 	}
 }
