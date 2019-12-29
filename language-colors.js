9292a3cb21d760603de6c7b092bcde9ae7dc97a3
Adewale Azeez <azeezadewale98@gmail.com>
2019-12-29 21:38:20 +0100

    test auto deploy to npm

--- language-colors.js
+++ language-colors.js
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	12/29/2019 9:34:02 PM
+	12/29/2019 9:38:08 PM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
