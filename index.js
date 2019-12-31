6ad7d3e529c246f282566ccd934ae33661f72068
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-01 00:06:12 +0100

    automate github and npm release

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	12/31/2019 11:56:10 PM
+	1/1/2020 12:05:23 AM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
