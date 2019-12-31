f2a8b3f78a953893c4b7601f365fffee596b5bcb
Adewale Azeez <azeezadewale98@gmail.com>
2019-12-31 23:24:51 +0100

    test release to github and update README

--- language-colors.css
+++ language-colors.css
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	12/31/2019 11:19:57 PM
+	12/31/2019 11:23:58 PM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
