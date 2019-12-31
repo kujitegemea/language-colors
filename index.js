c0193d525d10e08a2252e7fe5ef74df084ccb066
Adewale Azeez <azeezadewale98@gmail.com>
2019-12-31 23:30:40 +0100

    test release to github and update README

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	12/31/2019 10:27:35 PM
+	12/31/2019 11:29:41 PM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
