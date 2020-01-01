ecdcb6714ed7a4c708fdd76518db62c27a8296d1
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-01 16:14:49 +0100

    try npm access error from native-api/dpl branch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	1/1/2020 2:28:25 AM
+	1/1/2020 4:14:41 PM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
