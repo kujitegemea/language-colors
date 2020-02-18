a069515a6ccdd6aa2be7956527b4d4eaddafec52
Travis CI <azeezadewale98@gmail.com>
2020-02-18 09:17:08 +0000

    Travis build=169. Auto update from Travis CI

--- docs/language-colors.js
+++ docs/language-colors.js
@@ -2,7 +2,7 @@
 	Adewale Azeez <azeezadewale98@gmail.com>
 	MIT License
 	Copyright (c) 2019 Quick Utils
-	2/11/2020 9:17:05 AM
+	2/18/2020 9:17:02 AM
 **/
 /**THIS FILE IS AUTO GENERATED DO NOT MODIFY MANUALLY**/
 
